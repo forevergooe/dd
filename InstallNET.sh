428cacce807bc335de4e57a674ab238fa5eb420f
forevergooe <35247288+forevergooe@users.noreply.github.com>
2020-04-22 10:47:02 +0800

    Update InstallNET.sh

--- InstallNET.sh
+++ InstallNET.sh
@@ -321,7 +321,7 @@ if [[ "$SpikCheckDIST" == '0' ]]; then
 fi
 
 [[ "$ddMode" == '1' ]] && {
-  export SSL_SUPPORT='https://moeclub.org/get/wget_udeb_amd64';
+  export SSL_SUPPORT='https://raw.githubusercontent.com/forevergooe/dd/master/wget_udeb_amd64.tar.gz';
   if [[ -n "$tmpURL" ]]; then
     DDURL="$tmpURL"
     echo "$DDURL" |grep -q '^http://\|^ftp://\|^https://';
